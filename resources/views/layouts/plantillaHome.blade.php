be193db4ffd3f76172e083da49def9b2ef937193
Mateo Arboleda <teo.arb03@gmail.com>
2022-11-04 01:14:32 -0500

    commit home

--- resources/views/layouts/plantillaHome.blade.php
+++ resources/views/layouts/plantillaHome.blade.php
@@ -23,9 +23,9 @@
     <!-- header -->
     <!-- nav -->
     <div class="navbar">
-        <a class="active" href="{{route('home')}}"><i class="fa fa-fw fa-home"></i> Home</a>
-        <a href="{{route('course.index')}}"><i class="fa fa-fw fa-pencil"></i> Courses</a>
-        <a href="{{route('group.index')}}"><i class="fa fa-fw fa-book"></i> Groups</a>
+        <a class="active" href="{{ route('home') }}"><i class="fa fa-fw fa-home"></i> Home</a>
+        <a href="{{ route('course.index') }}"><i class="fa fa-fw fa-pencil"></i> Courses</a>
+        <a href="{{ route('group.index') }}"><i class="fa fa-fw fa-book"></i> Groups</a>
         <a href="#"><i class="fa fa-fw fa-envelope"></i> Contact</a>
         <a href="#"><i class="fa fa-fw fa-user"></i> Login</a>
     </div>
@@ -41,38 +41,48 @@
 </html>
 
 <style>
-        /* Style the navigation bar */
-.navbar {
-  width: 100%;
-  background-color: #555;
-  overflow: auto;
-}
+    body {
+        font-family: Arial, Helvetica, sans-serif;
+        margin: 0;
+        background: linear-gradient(0deg, rgb(2, 2, 2) 0%, rgb(255, 255, 255) 100%);
+    }
 
-/* Navbar links */
-.navbar a {
-  float: left;
-  text-align: center;
-  padding: 12px;
-  color: white;
-  text-decoration: none;
-  font-size: 17px;
-}
+    html {
+        box-sizing: border-box;
+    }
 
-/* Navbar links on mouse-over */
-.navbar a:hover {
-  background-color: #000;
-}
+    /* Style the navigation bar */
+    .navbar {
+        width: 100%;
+        background-color: #555;
+        overflow: auto;
+    }
 
-/* Current/active navbar link */
-.active {
-  background-color: #04AA6D;
-}
+    /* Navbar links */
+    .navbar a {
+        float: left;
+        text-align: center;
+        padding: 12px;
+        color: white;
+        text-decoration: none;
+        font-size: 17px;
+    }
 
-/* Add responsiveness - will automatically display the navbar vertically instead of horizontally on screens less than 500 pixels */
-@media screen and (max-width: 500px) {
-  .navbar a {
-    float: none;
-    display: block;
-  }
-}
+    /* Navbar links on mouse-over */
+    .navbar a:hover {
+        background-color: #000;
+    }
+
+    /* Current/active navbar link */
+    .active {
+        background-color: #04AA6D;
+    }
+
+    /* Add responsiveness - will automatically display the navbar vertically instead of horizontally on screens less than 500 pixels */
+    @media screen and (max-width: 500px) {
+        .navbar a {
+            float: none;
+            display: block;
+        }
+    }
 </style>
