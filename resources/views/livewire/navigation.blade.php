395231d9dc571cd48bb03968ab81bb5dc47a27f6
Mateo Arboleda <teo.arb03@gmail.com>
2022-11-29 20:52:56 -0500

    Se ocultan botones dependiendo del rol de usuario

--- resources/views/livewire/navigation.blade.php
+++ resources/views/livewire/navigation.blade.php
@@ -42,10 +42,15 @@
           <div class="hidden sm:ml-6 sm:block">
             <div class="flex space-x-4">
               <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-              <a href="{{ route('student.index') }}" class="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium">Pensum</a>
-  
-              <a href="{{ route('course.index') }}" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">My Courses</a>
+              
+              @can('student.index')
+                <a href="{{ route('student.index') }}" class="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium">Pensum</a>
+              @endcan
 
+              @can('course.index')
+                <a href="{{ route('course.index') }}" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">My Courses</a>
+              @endcan
+              
             </div>
           </div>
         </div>
@@ -66,7 +71,11 @@
                     <div x-show="open" x-on:click.away="open = false" class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
 
                         <a href="{{ route('profile.show') }}" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-0">Your Profile</a>
+
+
+                        @can('admin.home')
                         <a href="{{ route('admin.home') }}" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-0">Admin</a>
+                        @endcan
 
                         <form method="POST" action="{{ route('logout') }}" x-data>
                             @csrf
