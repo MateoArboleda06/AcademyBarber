57530d30af32165964f0295402051c0c80d6f132
Jose <david-restrepo_123@hotmail.es>
2022-11-29 23:27:59 -0500

    (feat): darkmode implementation

--- resources/views/livewire/navigation.blade.php
+++ resources/views/livewire/navigation.blade.php
@@ -1,4 +1,4 @@
-<nav style="background-color: #202020" x-data="{ open: false }">
+<nav style="background-color: #272727" x-data="{ open: false }">
     <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
         <div class="relative flex h-16 items-center justify-between">
 
@@ -94,7 +94,7 @@
                         </button>
                     </div> --}}
 
-                    <div class="relative ml-3">
+                    {{--                     <div class="relative ml-3">
                         <button class="switch" id="switch">
                             <label class="inline-flex relative items-center cursor-pointer">
                                 <input type="checkbox" value="" class="sr-only peer">
@@ -105,9 +105,28 @@
                                 </div>
                             </label>
                         </button>
+                    </div> --}}
+
+                    {{--                     <div class="relative ml-3">
+                        <label for="toggle" id="label_toggle">
+                            <i class="fa-solid fa-moon"></i>
+                        </label>
+                        <input type="checkbox" id="toggle">
+                    </div> --}}
+
+                    <div class="relative ml-3">
+                        
+                            <label class="inline-flex relative items-center cursor-pointer">
+                                <input type="checkbox" onclick="changemode()" value="" class="sr-only peer">
+                                <div
+                                    class="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600">
+                                </div>
+                                
+                            </label>
+                            
+                        
                     </div>
-
-
+                    <span class="mb-2 ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">Dark mode</span>
 
                     <!-- Profile dropdown -->
 
@@ -179,7 +198,7 @@
     </div>
 </nav>
 
-<script>
+{{-- <script>
     const btnSwitch = document.querySelector('#switch');
 
     btnSwitch.addEventListener('click', () => {
@@ -228,4 +247,4 @@
         background: none;
         color: #fff;
     }
-</style>
+</style> --}}
