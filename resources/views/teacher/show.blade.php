bd5ec7795b4d6c9b4314fcb9f822db691d4e0f31
Jose <david-restrepo_123@hotmail.es>
2022-12-14 00:03:28 -0500

    (feat): Se estiliza la vista admin, teacher y se agrega audio de bienvenida

--- resources/views/teacher/show.blade.php
+++ resources/views/teacher/show.blade.php
@@ -16,7 +16,8 @@
                 </div>
 
                 <div class="col-6">
-                    <button class="btn btn-rounded btn-success" id="eliminar_estudiantes" onclick="destroy({{ $group->id }}, {{ $students }})">End Course</button>
+                    <button class="button2" id="eliminar_estudiantes"
+                        onclick="destroy({{ $group->id }}, {{ $students }})">End Course</button>
                 </div>
             </div>
         </div>
@@ -41,7 +42,7 @@
                             <td>{{ $student->id }}</td>
                             <td>{{ $student->name }}</td>
                             <td>{{ $student->email }}</td>
-                        
+
                             @foreach ($records as $record)
                                 @if ($record->id_user == $student->id && $record->id_course == $group->course_id)
                                     <td>{{ $record->num_viewd }}</td>
@@ -51,9 +52,9 @@
                                 @if ($rating->id_user == $student->id && $rating->id_group == $group->id)
                                     <td>{{ $rating->status }}</td>
                                 @endif
-                            @endforeach 
-                            
-                            <td >
+                            @endforeach
+
+                            <td>
                                 <a class="button"
                                     href="{{ route('tracing', ['id' => $student, 'id_g' => $group]) }}">Tracing</a>
                             </td>
@@ -74,34 +75,33 @@
 <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
 
 <script>
-
-    function destroy($id_group, $students){
+    function destroy($id_group, $students) {
 
         event.preventDefault();
 
         $.ajax({
-                type: "POST",
-                url: "{{ route('end') }}",
-                headers: {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')},
-                data: {
-                    "id_group": $id_group,
-                    "list_students": $students
-                },
-                dataType: "json",
-                success: function(response){
+            type: "POST",
+            url: "{{ route('end') }}",
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            },
+            data: {
+                "id_group": $id_group,
+                "list_students": $students
+            },
+            dataType: "json",
+            success: function(response) {
 
-                    if(response == 'success'){
+                if (response == 'success') {
 
-                        window.location.reload();
+                    window.location.reload();
 
-                        return;
-                    }
-
+                    return;
                 }
-            });
+
+            }
+        });
     }
-
-
 </script>
 
 
@@ -111,7 +111,7 @@
         margin: 0;
         background: linear-gradient(0deg, rgba(84, 49, 27) 0%, rgb(255, 255, 255) 150%);
     }
-<style>
+
     .body {
         font-family: Arial, Helvetica, sans-serif;
         margin: 0;
@@ -123,13 +123,15 @@
         color: #f1eded;
         transition: all 1.5s ease;
     }
-    .wel{
+
+    .wel {
         color: black;
     }
 
     .navi {
         background-color: #272727;
     }
+
     .navi {
         background-color: #272727;
     }
@@ -146,36 +148,98 @@
         text-align: center;
     }
 
-        .anime {
-            animation-duration: 3s;
-            animation-name: slidein;
-            animation-iteration-count: infinite;
-            animation-direction: alternate;
+    .anime {
+        animation-duration: 3s;
+        animation-name: slidein;
+        animation-iteration-count: infinite;
+        animation-direction: alternate;
+    }
+
+    .h1 {
+        text-shadow:
+            3px 3px 1px #272727,
+            5px 5px 2px white,
+            8px 8px 3px rgba(84, 49, 27);
+        ;
+    }
+
+    .text-gradient {
+        color: transparent;
+        background-image: linear-gradient(to left, #272727, #3498db, rgba(84, 49, 27));
+        background-clip: text;
+        animation: color 5s linear infinite;
+        background-size: 500%;
+    }
+
+    @keyframes color {
+        from {
+            background-position: 0% 50%;
         }
 
-        .h1 {
-            text-shadow:
-                3px 3px 1px #272727,
-                5px 5px 2px white,
-                8px 8px 3px rgba(84, 49, 27);
-            ;
+        to {
+            background-position: 100% 50%;
+        }
+    }
+
+    .button {
+        background: linear-gradient(to bottom right, rgba(164, 120, 93), rgba(28, 27, 23));
+        border: 0;
+        border-radius: 12px;
+        color: #FFFFFF;
+        cursor: pointer;
+        display: inline-block;
+        font-family: -apple-system, system-ui, "Segoe UI", Roboto, Helvetica, Arial, sans-serif;
+        font-size: 16px;
+        font-weight: 500;
+        line-height: 2.5;
+        outline: transparent;
+        padding: 0 1rem;
+        text-align: center;
+        text-decoration: none;
+        transition: box-shadow .2s ease-in-out;
+        user-select: none;
+        -webkit-user-select: none;
+        touch-action: manipulation;
+        white-space: nowrap;
+    }
+
+    .button:not([disabled]):focus {
+        box-shadow: 0 0 .25rem rgba(0, 0, 0, 0.5), -.125rem -.125rem 1rem rgba(239, 71, 101, 0.5), .125rem .125rem 1rem rgba(255, 154, 90, 0.5);
+    }
+
+    .button:not([disabled]):hover {
+        box-shadow: 0 0 .25rem rgba(0, 0, 0, 0.5), -.125rem -.125rem 1rem rgba(239, 71, 101, 0.5), .125rem .125rem 1rem rgba(255, 154, 90, 0.5);
+    }
+    .button2 {
+            background: linear-gradient(to bottom right, rgb(106, 255, 0), rgba(28, 27, 23));
+            border: 0;
+            border-radius: 12px;
+            color: #FFFFFF;
+            cursor: pointer;
+            display: inline-block;
+            font-family: -apple-system, system-ui, "Segoe UI", Roboto, Helvetica, Arial, sans-serif;
+            font-size: 16px;
+            font-weight: 500;
+            line-height: 2.5;
+            outline: transparent;
+            padding: 0 1rem;
+            text-align: center;
+            text-decoration: none;
+            transition: box-shadow .2s ease-in-out;
+            user-select: none;
+            -webkit-user-select: none;
+            touch-action: manipulation;
+            white-space: nowrap;
+
+            width: auto;
+            height: auto;
         }
 
-        .text-gradient {
-            color: transparent;
-            background-image: linear-gradient(to left, #272727, #3498db, rgba(84, 49, 27));
-            background-clip: text;
-            animation: color 5s linear infinite;
-            background-size: 500%;
+        .button2:not([disabled]):focus {
+            box-shadow: 0 0 .25rem rgba(0, 0, 0, 0.5), -.125rem -.125rem 1rem rgba(239, 71, 101, 0.5), .125rem .125rem 1rem rgba(255, 154, 90, 0.5);
         }
 
-        @keyframes color {
-            from {
-                background-position: 0% 50%;
-            }
-
-            to {
-                background-position: 100% 50%;
-            }
+        .button2:not([disabled]):hover {
+            box-shadow: 0 0 .25rem rgba(0, 0, 0, 0.5), -.125rem -.125rem 1rem rgba(239, 71, 101, 0.5), .125rem .125rem 1rem rgba(255, 154, 90, 0.5);
         }
-    </style>
+</style>
